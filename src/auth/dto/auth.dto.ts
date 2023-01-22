d656e105af0b72deba4f32490e00f489aeecc8ba
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-01-22 16:45:13 +0900

    add validation provided by class calidator to auth dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,24 @@
-export interface AuthDto {
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  // IsStrongPasswordOptions = Pick<validator.StrongPasswordOptions, 'minLength' | 'minLowercase' | 'minUppercase' | 'minNumbers' | 'minSymbols'>;
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 0,
+    minSymbols: 0,
+  })
   password: string;
 }
