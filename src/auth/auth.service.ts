af14099122f2f6429fd96e5a04c01d9cb2d443ac
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-01-22 17:57:27 +0900

    add function to create test user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+import { AuthDto } from '@/auth/dto';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +10,20 @@ export class AuthService {
   login() {
     return { message: 'Logged in' };
   }
-  signup() {
-    return { message: 'Created new account' };
+
+  async signup(dto: AuthDto) {
+    /* generate password */
+    const hash = await argon2.hash(dto.password);
+    /* save the new user in DB */
+    const user = this.prismaService.user.create({
+      data: {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: dto.email,
+        password: hash,
+      },
+    });
+    /* return the saved user */
+    return user;
   }
 }
