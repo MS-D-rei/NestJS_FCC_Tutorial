a5ee08d2c60db06b9ce43cb93d3013ff067e462e
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-01-22 18:58:59 +0900

    add error handling to signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { PrismaService } from '@/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { AuthDto } from '@/auth/dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -15,15 +16,27 @@ export class AuthService {
     /* generate password */
     const hash = await argon2.hash(dto.password);
     /* save the new user in DB */
-    const user = this.prismaService.user.create({
-      data: {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: dto.email,
-        password: hash,
-      },
-    });
-    /* return the saved user */
-    return user;
+    try {
+      // don't forget to add await
+      const user = await this.prismaService.user.create({
+        data: {
+          firstName: 'John',
+          lastName: 'Doe',
+          email: dto.email,
+          password: hash,
+        },
+      });
+      /* return the saved user */
+      return user;
+    } catch (err) {
+      /* error handling */
+      // https://www.prisma.io/docs/concepts/components/prisma-client/handling-exceptions-and-errors
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credetials taken');
+        }
+      }
+      throw err;
+    }
   }
 }
