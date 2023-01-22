ae2d816626c1a9f1f47374a7faf795198ee896e7
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-01-22 23:37:24 +0900

    add jwt strategy class as auth module provider

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+// Official Docs
+// https://docs.nestjs.com/security/authentication#implementing-passport-jwt
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false, // this is set by default
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+}
