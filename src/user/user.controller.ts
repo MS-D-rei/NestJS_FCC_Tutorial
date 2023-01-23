733d7595e835f8c85d9bfc052dccdd9d8ef34d75
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-01-23 20:37:58 +0900

    add JwtGuard class

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
+import { JwtGuard } from '@/auth/guard';
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
   /* GET: /users/me */
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) {
     return req.user;
