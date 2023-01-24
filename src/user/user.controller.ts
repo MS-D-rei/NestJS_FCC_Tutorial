a20758498750e4531cb2b63346ff3c7abc90f939
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-01-24 15:16:19 +0900

    change GetUser decorator to get specific field info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,12 +3,14 @@ import { JwtGuard } from '@/auth/guard';
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 
+/* controller level UseGuard */
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   /* GET: /users/me */
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@GetUser() user: User) {
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log(email);
     return user;
   }
 }
