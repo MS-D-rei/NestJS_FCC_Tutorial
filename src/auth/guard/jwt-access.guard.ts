7a1817b0fbf2e39d7dc39c51a6c02cb194de8a04
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-01 20:53:53 +0900

    add logic whether metadata has isPublic true or not

--- src/auth/guard/jwt-access.guard.ts
+++ src/auth/guard/jwt-access.guard.ts
@@ -1,3 +1,31 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
-export class JwtAccessTokenGuard extends AuthGuard('jwt') {}
+/* NestJS Docs: Reflection and metadata */
+/* https://docs.nestjs.com/fundamentals/execution-context#reflection-and-metadata */
+
+@Injectable()
+export class JwtAccessTokenGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log(context.getHandler().name);
+    console.log(context.getClass());
+    /* extracting the metadata for the currently processed route handler and controller level */
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
