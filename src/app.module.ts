235fdeb158e59202f3094649c6dfafd21831ea85
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-02 20:41:12 +0900

    add LoggingInterceptor to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,9 @@ import { AuthModule } from '@/auth/auth.module';
 import { UserModule } from '@/user/user.module';
 import { BookmarkModule } from '@/bookmark/bookmark.module';
 import { PrismaModule } from '@/prisma/prisma.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAccessTokenGuard } from './auth/guard';
+import { LoggingInterceptor } from './interceptor/logging.interceptor';
 
 @Module({
   imports: [
@@ -26,6 +27,10 @@ import { JwtAccessTokenGuard } from './auth/guard';
     {
       provide: APP_GUARD,
       useClass: JwtAccessTokenGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
     },
   ],
 })
